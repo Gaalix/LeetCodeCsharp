41c6c199ae51be3e879e47ae77c3a084505c6e03
Gal <134967079+Gaalix@users.noreply.github.com>
2024-01-01 17:11:27 +0100

    Added Problem1 and TwoSum

---
+++ Problem1/LeetCode1/Program.cs
@@ -0,0 +1,29 @@
+﻿static int GetMax(int num1, int num2)
+{
+    return (num1 > num2) ? num1 : num2;
+}
+
+
+int[] nums = [-15, -6, -3, -2, 0, 0, 1, 2, 5, 6, 8, 9];
+int posNums = 0;
+int negNums = 0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] < 0)
+    {
+        negNums++;
+    }
+    else if (nums[i] > 0)
+    {
+        posNums++;
+    }
+}
+
+Console.WriteLine("Max is: " + GetMax(posNums, negNums).ToString());
+
+
+
+
+
+
---
+++ TwoSum/LeetCode2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] TwoSum(int[] nums, int target) {
+    int[] result = new int[2];
+    for (int i = 0; i < nums.Length; i++) {
+        for(int j = i + 1; j < nums.Length; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    result[0] = -1;
+    result[1] = -1;
+    return result;
+}
+
+
+int[] nums = [ 2, 7, 11, 15 ];
+int target = 70;
+
+int[] result = TwoSum(nums, target);
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
